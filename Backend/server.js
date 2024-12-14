e639ddc3d1cb96481437a3e1960b3fae921f4dc4
Alkaif <alkaifkhawarizmi@gmail.com>
2024-12-14 10:38:07 +0530

    starting

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,9 @@
-const http = require('http')
+const http = require("http")
 const app = require('./app')
 const port = process.env.PORT || 3000
 
 const server = http.createServer(app)
 
-server.listen(port , ()=>{
-  console.log("listening on port " + port)
+server.listen(port , () => {
+  console.log(`Server running on port ${port}`)
 })
